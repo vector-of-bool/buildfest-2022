ec07fd849d25468ed1cfc68e6c82e74925046e3c
Ross Lawley <ross.lawley@gmail.com>
2022-09-20 10:17:55 +0100

    Update readme to include .env.local and add css framework

--- node/mo/README.md
+++ node/mo/README.md
@@ -12,6 +12,13 @@ yarn dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
+Add a local environment settings file: `.env.local`
+
+```
+DATABASE_URL=mongodb://localhost:27017/mo_links
+API_URL=http://localhost:3000/api/links
+```
+
 You can start editing the page by modifying `pages/index.tsx`. The page auto-updates as you edit the file.
 
 [API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.ts`.
@@ -24,6 +31,7 @@ To learn more about Next.js, take a look at the following resources:
 
 - [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
 - [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+- [Bulma css](https://bulma.io/) - Bulma: the modern CSS framework that just works.
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
--- node/mo/pages/index.tsx
+++ node/mo/pages/index.tsx
@@ -1,5 +1,7 @@
+import 'bulma/css/bulma.min.css';
 import { MoLink } from "../utils/types"
 import Link from "next/link"
+
 
 // Define the components props
 interface IndexProps {
@@ -11,19 +13,26 @@ function Index(props: IndexProps) {
   const { links } = props
 
   return (
-      <div>
-        <h1>Mo Links</h1>
-        <h2>Click On Link to see it individually</h2>
-        {/* MAPPING OVER THE MOLINKS */}
-        {links.map(t => (
-            <div key={t._id}>
-              <Link href={`/links/${t._id}`}>
-                <h3 style={{ cursor: "pointer" }}>
-                  {t.alias} - {t.link}
-                </h3>
-              </Link>
-            </div>
-        ))}
+      <div className="section">
+        <div className="container">
+          <h1 className="title">
+            Mo Links
+          </h1>
+          <p className="subtitle">
+            Click On Link to see it individually
+          </p>
+        </div>
+        <div className="container"></div>
+         {/* MAPPING OVER THE MOLINKS */}
+         {links.map(t => (
+          <div key={t._id}>
+            <Link href={`/links/${t._id}`}>
+              <h3 style={{ cursor: "pointer" }}>
+                {t.alias} - {t.link}
+              </h3>
+            </Link>
+          </div>
+          ))}
       </div>
   )
 }
